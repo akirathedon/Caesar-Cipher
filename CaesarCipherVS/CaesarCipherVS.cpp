095c3dcc9495e931c8e7a44af35757b61b5c2a67
akirathedon <43752772+akirathedon@users.noreply.github.com>
2020-01-09 12:03:43 +0000

    Add files via upload

---
+++ CaesarCipherVS/CaesarCipherVS.cpp
@@ -0,0 +1,35 @@
+// CaesarCipherVS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string input;
+	int count = 0, length;
+
+	cout << "Enter your phrase: \n";
+	getline(cin, input);
+
+	length = (int)input.length();
+
+	for (count = 0; count < length; count++)
+	{
+		if (isalpha(input[count]))
+		{
+			input[count] = tolower(input[count]);
+			for (int i = 0; i < 13; i++)
+			{
+				if (input[count] == 'z')
+					input[count] = 'a';
+				else
+					input[count]++;
+			}
+		}
+	}
+
+	cout << "Results: \n" << input << endl;
+}
